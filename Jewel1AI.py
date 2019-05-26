19ec3f2e027546e25e26a4af6eea3e780fdfe24d
Joseph Woolf <joseph@josephwoolf.com>
2019-05-25 18:01:06 -0700

    Completed implementation of getting the game board.

--- Jewel1AI.py
+++ Jewel1AI.py
@@ -9,6 +9,11 @@ import time
 import numpy as np
 import pytesseract
 
+
+def showImage(title, img):
+    cv2.imshow(title, img)
+    cv2.waitKey(0)
+
 class Jewel1AI:
     # Holds the class information about the AI
     def __init__(self):
@@ -42,7 +47,69 @@ class Jewel1AI:
         # Responsible for getting the information
         img = self.getWindowShot()
         (x, y, w, h) = self._getPlayingFieldCoord(img)
-        return img[y:y+h, x:x+w]
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # The ordering for the limits follows:
+        # 1) Red
+        # 2) White
+        # 3) Yellow
+        # 4) Blue
+        # 5) Purple
+        # 6) Orange
+        # 7) Green
+        croppedImage = img[y:y+h, x:x+w]
+        colors = [
+            "R",
+            "W",
+            "Y",
+            "B",
+            "P",
+            "O",
+            "G"
+        ]
+        lowerLimits = [
+            np.array([0, 0, 128]),
+            np.array([200, 200, 200]), 
+            np.array([0, 128, 128]),
+            np.array([128, 128, 0]),
+            np.array([128, 0, 128]),
+            np.array([0, 64, 170]),
+            np.array([32, 150, 32])
+        ]
+        higherLimits = [
+            np.array([0, 0, 255]),
+            np.array([255, 255, 255]), 
+            np.array([0, 255, 255]),
+            np.array([255, 255, 0]),
+            np.array([255, 0, 255]),
+            np.array([0, 128, 255]),
+            np.array([150, 255, 150])
+        ]
+        matrix = [
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0]
+        ]
+        for multipleY in range(1,9):
+            for multipleX in range(1,9):
+                imgSeg = croppedImage[12 + (52*(multipleY-1)):12 + (52*multipleY), 12 + (52*(multipleX-1)):12 + (52*(multipleX))]
+                highestCount = 0
+                theColor = "N/A"
+                showImage("Image", imgSeg)
+                for (lower, higher, color) in zip(lowerLimits, higherLimits, colors):
+                    filteredImg = cv2.inRange(imgSeg, lower, higher)
+                    uniques, counts = np.unique(filteredImg, return_counts=True)
+                    counts = dict(zip(uniques, counts))
+                    if 255 in counts and counts[255] > highestCount:
+                        highestCount = counts[255]
+                        theColor = color
+                matrix[multipleY-1][multipleX-1] = theColor
+
+        return matrix
 
     def handleTitleScreen(self):
         canPlayNow = False
